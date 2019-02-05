c57703f747a16c98e4a165cd390f9533cfc80f74
steelworx <eileencuevas62@gmail.com>
2019-02-04 21:40:27 -0500

    added development configuration to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,24 @@
 // Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/safeSpace.sqlite3'
+      filename: './data/safeSpace.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/safeSpace.sqlite3',
     },
     useNullAsDefault: true,
     migrations: {
