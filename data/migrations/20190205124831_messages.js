37fabc58654bf56bdde53bd485d90f458a8c6802
steelworx <eileencuevas62@gmail.com>
2019-02-05 13:12:20 -0500

    created messages table using knex

---
+++ data/migrations/20190205124831_messages.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('messages', table => {
+      table.increments();
+      table
+        .string('text', 255)
+        .notNullable();
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('users.id');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('messages');
+};
