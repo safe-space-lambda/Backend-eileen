8acbb54e9796643b2a059e6e6750eaf2e519d1f1
steelworx <eileencuevas62@gmail.com>
2019-02-04 21:10:57 -0500

    adds GET, PUT, DELETE endpoints for users

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const middleware = require('./config/middlewareConfig');
 const bcrypt = require('bcryptjs');
 const helpers = require('../data/helpers/helperFunctions');
+const restricted = require('./middleware/restricted');
+const restrictedByUser = require('./middleware/restrictedByUser');
 
 const server = express();
 
@@ -38,13 +40,19 @@ server.post('/api/login', async (req, res) => {
 
     if (creds.username) {
         if (creds.password) {
-            let response = await helpers.beginLogin(creds);
+            try {
+                let response = await helpers.beginLogin(creds);
 
-            if (!response || !bcrypt.compareSync(creds.password, response.password)) {
-                res.status(401).json({ message: `Invalid username or password.` });
-            } else {
-                const token = helpers.generateToken(response);
-                res.status(200).json(token);
+                if (!bcrypt.compareSync(creds.password, response.password)) {
+                    res.status(401).json({ message: `Invalid username or password.` });
+                } else {
+                    const token = helpers.generateToken(response);
+                    res.status(200).json(token);
+                }
+            } catch (err) {
+                res.status(500).json({
+                    error: `Couldn't log in. Please try again.`
+                });
             }
         } else {
             res.status(403).json({
@@ -57,5 +65,54 @@ server.post('/api/login', async (req, res) => {
         })
     }
 });
+
+server.get('/api/users/', restricted, async (req, res) => {
+    try {
+        let response = await helpers.getUsers();
+
+        res.status(200).json(response);
+    } catch (err) {
+        res.status(500).json({
+            error: `Couldn't access user data. Please try again.`
+        });
+    }
+});
+
+server.get('/api/users/:id', restricted, async (req, res) => {
+    try {
+        let response = await helpers.getUserById(req.params.id);
+
+        res.status(200).json(response);
+    } catch (err) {
+        res.status(500).json({
+            error: `Couldn't access user data. Please try again.`
+        });
+    }
+});
+
+server.put('/api/users/:id', restrictedByUser, async (req, res) => {
+    try {
+        let response = await helpers.updateUser(req.params.id, req.body);
+
+        res.status(200).json(response);
+    } catch (err) {
+        res.status(404).json({
+            error: `Could not find specified user.`
+        });
+    }
+});
+
+server.delete('/api/users/:id', restrictedByUser, async(req, res) => {
+    try {
+        let response = await helpers.deleteUser(req.params.id);
+        
+        req.decodedToken = null;
+        res.status(200).json(response);
+    } catch (err) {
+        res.status(404).json({
+            error: `Could not find specified user.`
+        });
+    }
+})
 
 module.exports = server;
