9977234c07f15bff800d23a11ae8879803b816cd
steelworx <eileencuevas62@gmail.com>
2019-02-06 12:59:32 -0500

    added phone numbers to seed data

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -7,9 +7,24 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {name: 'John', username: 'JohnDoe', password: bcrypt.hashSync('test1', 16)},
-        {name: 'Jess', username: 'JessDoe', password: bcrypt.hashSync('test1', 16)},
-        {name: 'Rebecca', username: 'tester',  password: bcrypt.hashSync('test1', 16)}
+        {
+          name: 'John', 
+          username: 'JohnDoe', 
+          password: bcrypt.hashSync('test1', 16),
+          phoneNumber: bcrypt.hashSync('18008888888', 16)
+        },
+        {
+          name: 'Jess', 
+          username: 'JessDoe', 
+          password: bcrypt.hashSync('test1', 16),
+          phoneNumber: bcrypt.hashSync('15185550181', 16)
+        },
+        {
+          name: 'Rebecca', 
+          username: 'tester',  
+          password: bcrypt.hashSync('test1', 16),
+          phoneNumber: bcrypt.hashSync('15185550195', 16)
+        }
       ]);
     });
 };
