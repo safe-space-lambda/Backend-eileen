a059ab16362b90a2ae44e5f75247dad14302ae90
steelworx <eileencuevas62@gmail.com>
2019-02-07 01:10:19 -0500

    imported twilioScheduled so that it can be used

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const server = require('./api/server');
+const runScheduledTexts = require('./api/twilio/twilioScheduled');
+
+runScheduledTexts();
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\nServer running on port ${port}\n`));
