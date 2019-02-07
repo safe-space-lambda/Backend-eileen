34ae4df8241e6da6b7bba487e3306b8f3c773529
steelworx <eileencuevas62@gmail.com>
2019-02-07 01:06:24 -0500

    Revert "added route for twilio-related endpoint"
    This reverts commit ac64fbebcb21fd5fb10b9e96d4732307d420b09f.

--- api/server.js
+++ api/server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const middleware = require('./config/middlewareConfig');
 const usersRouter = require('./users/usersRouter');
 const messagesRouter = require('./messages/messagesRouter');
-const twilioRouter = require('./twilio/twilioRouter');
+
 const server = express();
 
 middleware(server);
 
 server.use('/api/', usersRouter);
 server.use('/api/', messagesRouter);
-server.use('/api/messages/send/', twilioRouter);
 
 module.exports = server;
