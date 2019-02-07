7918decc7c2b9186467895c1045d9f6044df0ba4
steelworx <eileencuevas62@gmail.com>
2019-02-07 17:26:51 -0500

    removed runScheduledTexts function from index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const server = require('./api/server');
-const runScheduledTexts = require('./api/twilio/twilioScheduled');
-
-runScheduledTexts();
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\nServer running on port ${port}\n`));
