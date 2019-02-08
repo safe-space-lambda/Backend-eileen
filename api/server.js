64a7a746330759b3e695fd19457338806f4fabe4
steelworx <eileencuevas62@gmail.com>
2019-02-08 01:01:30 -0500

    added twilioRouter back into server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const middleware = require('./config/middlewareConfig');
 const usersRouter = require('./users/usersRouter');
 const messagesRouter = require('./messages/messagesRouter');
+const twilioRouter  = require('./twilio/twilioRouter');
 
 const server = express();
 
@@ -9,5 +10,6 @@ middleware(server);
 
 server.use('/api/', usersRouter);
 server.use('/api/', messagesRouter);
+server.use('/api/twilio', twilioRouter);
 
 module.exports = server;
